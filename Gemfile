fae9c4ec0114d7e1dc6ea376fb0bfaf517ab2421
Josh Wills <jcwills369@gmail.com>
2023-08-11 18:49:09 +0100

    index, show and new pages with bootstrap styling

--- Gemfile
+++ Gemfile
@@ -67,3 +67,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem "simple_form"
+gem "faker"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Flat.create!(
+    {
+      name: "#{Faker::Adjective.positive.capitalize} & #{Faker::Adjective.positive.capitalize} #{Faker::Address.city_prefix} Flat London",
+      address: ['31 West Street
+        London
+        SW54 3LM', '9446 George Street
+        London
+        SE58 7ZF', '9214 North Road
+        London
+        NW58 3VD', '89 Park Avenue
+        London
+        W98 6NV', '79 Park Lane
+        London
+        W73 2SK', '87 Main Road
+        London
+        SE13 5KV', '64 Park Road
+        London
+        SW75 4OK', '77 London Road
+        London
+        SW57 8UR'].sample,
+      description: Faker::Quote.mitch_hedberg,
+      price_per_night: rand(50..150),
+      number_of_guests: rand(2..8)
+    }
+  )
+end
